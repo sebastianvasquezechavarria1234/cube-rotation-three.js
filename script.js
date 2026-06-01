9c298d17e3da2264921187a36d2cf0a0382c6d59
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-06-01 15:10:33 -0500

    feat: add wireframe cube animation and improve camera positioning

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js';
 
-console.log("hola")
+const scene = new THREE.Scene();
 
+const fov = 70;
+const aspectRatio = window.innerWidth / window.innerHeight;
+const near = 0.1;
+const far = 1000;
 
-//scene
+const camera = new THREE.PerspectiveCamera(
+  fov,
+  aspectRatio,
+  near,
+  far
+);
 
-const scene = new THREEE.scene();
-
-
-//camara 
-const fev= 70;
-const near= 70;
-const far= 70;
-
-const camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
-
-//renderer
+camera.position.z = 8;
+camera.position.x = 3;
+camera.position.y = 3;
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.setSize(
+  window.innerWidth,
+  window.innerHeight
+);
+
 document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+const material = new THREE.MeshBasicMaterial({
+  color: "white",
+  wireframe: true,
+});
+
+const cube = new THREE.Mesh(
+  geometry,
+  material
+);
+
+scene.add(cube);
+
+camera.lookAt(cube.position);
+
+renderer.render(scene, camera);
+
+
+function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
