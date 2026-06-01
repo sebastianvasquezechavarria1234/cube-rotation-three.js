e8cae7d59d9c79cd25f8accf049db828be820b8a
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-06-01 14:56:02 -0500

    feat: initialize basic Three.js scene, camera and renderer setup

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-  import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js';
 
 console.log("hola")
+
+
+//scene
+
+const scene = new THREEE.scene();
+
+
+//camara 
+const fev= 70;
+const near= 70;
+const far= 70;
+
+const camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
+
+//renderer
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
